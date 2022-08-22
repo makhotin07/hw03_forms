58b2b0e41379621984bcde2e73b3fadf1368d6c3
Nikita Makhotin <makhotin.07@gmail.com>
2022-08-22 17:05:16 +0300

    pep83

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,4 @@
 from django.core.paginator import Paginator
-from django.shortcuts import get_list_or_404
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.shortcuts import render
